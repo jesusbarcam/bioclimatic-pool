180b5a4de9c1002c9fd1414416e62f7000552144
jesus Barajas Camacho <jesusbarcam@gmail.com>
2018-05-26 20:22:20 +0200

    Added and implemented ngx-translate library like dependency

--- src/app/modules/pools/pools.module.ts
+++ src/app/modules/pools/pools.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
 import { PoolsSectionComponent } from './components/pools-section/pools-section.component';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [PoolsSectionComponent],
   exports: [PoolsSectionComponent]
