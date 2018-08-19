cb699152a2b54046b860acdbb80f93bc132706c5
jesusbarcam <jesusbarcam@gmail.com>
2018-08-19 20:16:39 +0200

    Fix url of clickOutside directive in application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { PoolsModule } from './modules/pools/pools.module';
 import { PoolsSectionComponent } from './modules/pools/components/pools-section/pools-section.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ClickoutsideComponent } from './directives/clickoutside/clickoutside.component';
 
 
 // AoT requires an exported function for factories
@@ -38,8 +37,7 @@ export function createTranslateLoader(http: HttpClient) {
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    ClickoutsideComponent
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
